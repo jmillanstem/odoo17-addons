4b959ee2b0f8092091d8d7819295a3b56a3ac1cd
jmillanstem <jmillan@stemdo.io>
2024-07-04 12:21:12 +0200

    Initial commit with addons folder

---
+++ custom_module/__manifest__.py
@@ -0,0 +1,15 @@
+ 
+{
+    'name': 'Custom Module',
+    'version': '1.0',
+    'summary': 'A custom module for Odoo 17',
+    'description': 'Description of your custom module',
+    'author': 'Your Name',
+    'category': 'Uncategorized',
+    'depends': ['base'],
+    'data': [
+        'views/custom_view.xml',
+    ],
+    'installable': True,
+    'application': False,
+}
---
+++ custom_module/models/__init__.py
@@ -0,0 +1,2 @@
+ 
+from . import custom_model
